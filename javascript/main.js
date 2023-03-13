3679d1ccda212f1661731801633acb2e3ff3b073
Sirjamo1 <jam_how@hotmail.com>
2023-03-13 12:04:21 +1000

    mobile classes sction complete

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,10 @@
 const createHeader = () => {
     const toggleNavDisplay = (e) => {
-     e.stopPropagation()
+        e.stopPropagation();
         navList.classList.toggle("show-mobile-nav");
     };
-    const header = document.getElementById("header-placeholder");
+    const header = document.getElementsByTagName("header")[0];
+    console.log(header);
     const logoContainer = document.createElement("article");
     logoContainer.setAttribute("class", "logo-container");
     const heartHomeLink = document.createElement("a");
@@ -13,7 +14,7 @@ const createHeader = () => {
     heartHomeLink.appendChild(heartLogo);
     logoContainer.appendChild(heartHomeLink);
     const logoTitle = document.createElement("h4");
-    logoTitle.appendChild(document.createTextNode("ABC GYMS"));
+    logoTitle.appendChild(document.createTextNode("ML Strength"));
     logoContainer.appendChild(logoTitle);
     const logoSubTitle = document.createElement("h5");
     logoSubTitle.appendChild(document.createTextNode("Pump your heart!"));
@@ -44,49 +45,67 @@ const createHeader = () => {
     listLinkAbout.appendChild(document.createTextNode("About"));
     listItemAbout.appendChild(listLinkAbout);
     navList.appendChild(listItemAbout);
-    const listItemSportsServices = document.createElement("li");
-    const listLinkSportsServices = document.createElement("a");
-    listLinkSportsServices.setAttribute("href", "../html/sportsservices.html");
-    listLinkSportsServices.appendChild(
-        document.createTextNode("Sports Services")
-    );
-    listItemSportsServices.appendChild(listLinkSportsServices);
-    navList.appendChild(listItemSportsServices);
-     const listItemForm = document.createElement("li");
-     const listLinkForm = document.createElement("a");
-     listLinkForm.setAttribute("href", "../html/form.html");
-     listLinkForm.appendChild(
-         document.createTextNode("Book an appointment")
-     );
-     listItemForm.appendChild(listLinkForm);
-     navList.appendChild(listItemForm);
-      const listItemPrivacy = document.createElement("li");
-      const listLinkPrivacy = document.createElement("a");
-      listLinkPrivacy.setAttribute("href", "../html/privacy.html");
-      listLinkPrivacy.appendChild(document.createTextNode("Privacy"));
-      listItemPrivacy.appendChild(listLinkPrivacy);
-      navList.appendChild(listItemPrivacy);
+    const listItemClasses = document.createElement("li");
+    const listLinkClasses = document.createElement("a");
+    listLinkClasses.setAttribute("href", "../html/classes.html");
+    listLinkClasses.appendChild(document.createTextNode("Classes"));
+    listItemClasses.appendChild(listLinkClasses);
+    navList.appendChild(listItemClasses);
+    const listItemForm = document.createElement("li");
+    const listLinkForm = document.createElement("a");
+    listLinkForm.setAttribute("href", "../html/form.html");
+    listLinkForm.appendChild(document.createTextNode("Book an appointment"));
+    listItemForm.appendChild(listLinkForm);
+    navList.appendChild(listItemForm);
+    const listItemPrivacy = document.createElement("li");
+    const listLinkPrivacy = document.createElement("a");
+    listLinkPrivacy.setAttribute("href", "../html/privacy.html");
+    listLinkPrivacy.appendChild(document.createTextNode("Privacy"));
+    listItemPrivacy.appendChild(listLinkPrivacy);
+    navList.appendChild(listItemPrivacy);
     navContainer.appendChild(navList);
     header.appendChild(navContainer);
 };
 createHeader();
 
 const createFooter = () => {
-    const footerContainer = document.getElementById("footer-placeholder");
-    const leftSection = document.createElement("section");
+    const footerContainer = document.getElementsByTagName("footer")[0];
     const companyText = document.createElement("p");
-    companyText.appendChild(document.createTextNode("Company name etc."));
-    leftSection.appendChild(companyText);
-    footerContainer.appendChild(leftSection);
+    companyText.innerHTML = `ML Strength &copy ${new Date().getFullYear()}`;
+    footerContainer.appendChild(companyText);
     const rightSection = document.createElement("section");
     const developerText = document.createElement("p");
     developerText.appendChild(
         document.createTextNode("James Howells #467957957")
     );
-    rightSection.appendChild(developerText);
-    footerContainer.appendChild(rightSection);
+    footerContainer.appendChild(developerText);
 };
-createFooter()
+createFooter();
+const showClassTable = (classesButtons, i) => {
+    for (let j = 0; j < classesButtons.length; j += 1) {
+        classesButtons[j].classList.remove("classes-button-selected");
+        const tableToAddClass = document.getElementsByClassName(
+            `${classesButtons[j].id}-container`
+        );
+        tableToAddClass[0].classList.add("table-hidden");
+    }
+    classesButtons[i].classList.add("classes-button-selected");
+    const tableContainer = document.getElementsByClassName(
+        `${classesButtons[i].id}-container`
+    );
+    tableContainer[0].classList.toggle("table-hidden");
+};
+
+const addEventToButtons = () => {
+    const classesButtons = document.getElementsByClassName("classes-button");
+    for (let i = 0; i < classesButtons.length; i += 1) {
+        classesButtons[i].addEventListener("click", () =>
+            showClassTable(classesButtons, i)
+        );
+    }
+};
+addEventToButtons();
+
 //Remove show-mobile-nav class (if it was open when resizing screen)
 window.addEventListener("resize", () => {
     const navList = document.getElementsByClassName("nav-list");
